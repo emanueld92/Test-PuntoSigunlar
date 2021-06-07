a7cf80676850ef1c1472fa7f098d45fa676791e8
¨emanueld92¨ <emanuelX92@gmail.com>
2021-06-07 02:06:44 -0400

    Morse Listo

--- Morse/Traductor_morse.py
+++ Morse/Traductor_morse.py
@@ -1,56 +1,37 @@
-
-Alfabeto={'A': '.-', 'B':'-...','C':'-.-.', 'D':'-..','E':'.','F':'..-.','G':'--.','H':'....',
-          'I':'..','J':'.---','K':'-.-','L':'.-..','M':'--','N':'-.','O':'---','P':'.--.',
-          'Q':'--.-','R':'.-.','S':'...','T':'-','U':'..-','V':'...-','W':'.--','X':'-..-',
-          'Y':'-.--','Z':'--..','1':'.----','2':'..---','3':'...--','4':'....-','5':'.....',
-          '6':'-....','7':'--...','8':'---..','9':'----.','0':'-----'}
+Alfabeto = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+            'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+            'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+            'Y': '-.--', 'Z': '--..', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+            '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----'}
 
 
 def codifcar(mensaje):
-    traduccion=[]
+    traduccion = []
     for i in mensaje.upper():
         if i in Alfabeto:
-            traduccion.append(Alfabeto[i]+ ' ')
+            traduccion.append(Alfabeto[i] + ' ')
         elif i == ' ':
             traduccion.append('  ')
-    return ''.join(traduccion)     
+    return ''.join(traduccion)
 
-#.... --- .-.. .-   -- ..- -. -.. ---   . -.   -- --- .-. ... .   ... --- -.--   . -- .- -. ..- . .-..
+
 def decodificar(mensaje):
-    traduccion=[]
-    a= mensaje.split(' ')
-    espacios=[]
-    conta=0
-    #invertir diccionario para buscar clave valor en codigo morse
-    alfabeto_morse={v: k for k,v in Alfabeto.items()}
-    for l in mensaje:
-        if l==' ':
-           espacios.append(conta)
-        conta+=1 
-    
-    print(espacios)
-    
-    for i in a:
+    traduccion = []
+    # invertir diccionario para buscar clave valor en codigo morse
+    alfabeto_morse = {value: key for key, value in Alfabeto.items()}
+    for i in mensaje.split(' '):
         if i in alfabeto_morse:
             traduccion.append(alfabeto_morse[i])
+        else:
+            traduccion.append(' ')
         
         
-    return ' '.join(traduccion)
+    return ''.join(traduccion)
 
-        
-    
-msj= input('introduzca palabra a codificar/decodificar: ')
 
-if msj[0]=='.' or msj[0]=='-':
+msj = input('introduzca palabra a codificar/decodificar: ')
+
+if msj[0] == '.' or msj[0] == '-':
     print(decodificar(msj))
 else:
-    
-    print(codifcar(msj))    
-
-        
-
-    
-    
-    
-    
-
+    print(codifcar(msj))
